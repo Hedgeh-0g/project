35f1d04753dd9231781e9168b04463f18e242b79
Marina <marina.nadeyina@gmail.com>
2020-08-20 16:33:58 +0300

    Change js file add movies DB

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 'use srtrict';
 
-let answers =[];
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '0');
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+ let lastMovie = prompt('Один из последних просмотренных фильмов?', '');
+ let lastMovieRaiting = prompt('На сколько его оцените?', '');
 
-answers[0] = prompt('Как ваше имя?', '');
-answers[1] = prompt('Как ваша фамилия?', '');
-answers[2] = prompt('Сколько вам лет?', '');
+ let lastMovie2 = prompt('Один из последних просмотренных фильмов?', '');
+ let lastMovieRaiting2 = prompt('На сколько его оцените?', '');
 
-document.write(answers);
+ personalMovieDB.movies[lastMovie] = lastMovieRaiting;
+ personalMovieDB.movies[lastMovie2] = lastMovieRaiting2;
+
+console.log(personalMovieDB);
+
